7ddd8a1fed3cd043e612ac7512f3afe3fab3e4e1
Darek <darek95asz@gmail.com>
2019-09-20 20:37:41 +0200

    init

---
+++ Circle/circle/Circle.py
@@ -0,0 +1,33 @@
+from math import pi
+
+
+class Circle:
+    def __init__(self, radius=1):
+        self._radius = radius
+
+    def __repr__(self):
+        return f'Circle({self.radius})'
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value < 0:
+            raise ValueError("Radius cannot be negative")
+        self._radius = value
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, value):
+        self.radius = value / 2
+
+    @property
+    def area(self):
+        return pi * self.radius ** 2
+
+
